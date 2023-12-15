0b44b415521b1fcc6a02122926443520d062c0f2
bpena707 <46693930+bpena707@users.noreply.github.com>
2023-12-15 12:29:34 -0800

    image and styling
    added an image and styling to the gameover screen. also included a button that will start a new game

--- Screens/GameOverScreen.js
+++ Screens/GameOverScreen.js
@@ -1,14 +1,55 @@
 //shows once the game is over.
 
-import {View, Text} from "react-native";
+import {View, Text, Image, StyleSheet} from "react-native";
+import Title from "../components/ui/Title";
+import Colors from "../constants/colors";
+import PrimaryButton from "../components/ui/PrimaryButton";
+
 
 function GameOverScreen() {
     return (
-        <View>
-            <Text>GAME OVER!</Text>
+        <View style={styles.rootContainer}>
+            <Title>GAME OVER</Title>
+            <View style={styles.imageContainer}>
+                <Image style={styles.image} source={require('../assets/Images/success.png')} />
+            </View>
+            <Text style={styles.summaryText}>Your phone needed <Text style={styles.highlight}>X</Text> rounds to guess the number <Text style={styles.highlight}>Y</Text> </Text>
+            <PrimaryButton>START NEW GAME</PrimaryButton>
         </View>
     )
 
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        padding: 24,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    imageContainer: {
+        height: 300,
+        width: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: Colors.primary800,
+        overflow: 'hidden',
+        margin: 36,
+    },
+    image: {
+        height: '100%',
+        width: '100%',
+    },
+summaryText: {
+        fontFamily: 'open-sans',
+        fontSize: 24,
+        textAlign: 'center',
+        marginBottom: 24,
+    },
+    highlight: {
+        color: Colors.primary500,
+        fontFamily: 'open-sans-bold',
+    }
+})
